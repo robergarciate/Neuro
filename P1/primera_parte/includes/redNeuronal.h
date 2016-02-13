804aa093bc51c4cef078dc2ced41c81022d526ed
made1993 <mariogr_93@hotmail.com>
2016-02-13 16:03:54 +0100

    una posible implemetacion de test y train

---
+++ P1/primera_parte/includes/redNeuronal.h
@@ -0,0 +1,76 @@
+#include "neurona.h"
+
+typedef struct{
+	int entradas;		/*Neuronas en la capa de entrada*/
+	int salidas;		/*Neuronas en la capa de salida*/
+	int ocultas			/*Neuronas en la capa oculta*/
+	double tasa;		/*tasa de aprendizaje*/
+	neurona* neuronas; 	/*QUE NO SE NOS PASE QUE AQUI ESTA LA NEURONA DE SESGO*/
+}redNeuronal;
+
+
+/**
+***
+***
+***	El segundo argumento es un puntero a funcion, no se si esta bien escrito,
+***	en concreto se usan las distintas funciones de actualizacion de neurona.
+***
+***	Esta funcion tiene que actualizar desde las neuronas de entrada
+***	hasta las de salida, lo contrario es para los pesos.
+***
+***
+***
+**/
+int actualizaSalida(redNeuronal* red, double (*fActualizacion)(), double* entrada);
+
+/**
+***
+***	Tener ojo con esto y actulizar pesos de forma adecuada en funcion 
+***	de si es un perceptron o una red de Adaline
+***
+**/
+int actualizaPesos(redNeuronal* red);
+
+
+
+/**
+***	No hay mucha diferencia entre una inicializacion y otra.
+***	Si despues hacemos graficas sobre la valocidad con la 
+***	que aprende una red y otra probar si alguna inicializacion
+***	es mejor.
+**/
+
+/**
+***
+***	Inicializa toda la red y da valores a los pesos iniciales y a los sesgos
+***	En las traspas pone que inicializa a cero.
+***
+***
+**/
+int iniRedPerceptron(redNeuronal* red, int entrada, int salida, int oculta, double tasa);
+
+
+/**
+***
+***	Inicializa toda la red y da valores a los pesos iniciales y a los sesgos
+***	En las traspas pone que inicializa aleatorio con valores pequeños.
+***
+***
+**/
+int iniRedAdaline(redNeuronal* red, int entrada, int salida, int oculta, double tasa);
+
+/**
+***
+***	Esta funcion devuelve la red ya entrenada
+***
+***
+**/
+redNeuronal* redTrain(datos* data, int (*fini)(), int (*fsalida)(), int nentreada, int nsalida, int noculta);
+
+/**
+***
+***	Esta funcion devuelve las salidas esperadas de unos datos.
+***
+***
+**/
+int* redTest(datos* data, int (*fsalida)());
