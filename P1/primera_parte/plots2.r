c360be143063c1fe8ff4a8622f13c22be53e6af5
made1993 <mariogr_93@hotmail.com>
2016-02-28 15:42:50 +0100

    commit makefile

--- P1/primera_parte/plots2.r
+++ P1/primera_parte/plots2.r
@@ -18,7 +18,7 @@ legend(x= 600 , y=0.75*(max(t[,1][2:999])-min(t[,1][2:999])) ,lty=c(1,1),lwd=c(2
 
 
 
-plot(ta[,1], type="l", col="blue", ylim=c(0,1), ylab="adaptacion", xlab="etapas", main="perceptron")
+plot(ta[,1], type="l", col="blue", ylim=c(0,1), ylab="adaptacion", xlab="etapas", main="adaline")
 lines(ta1[,1], col="red")
 legend(x= 6000 , y= 0.75,lty=c(1,1),lwd=c(2.5,2.5),col=c("blue","red"), legend=c("tasa=0.1", "tasa=0.01"))
 
