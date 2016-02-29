58ae10d9adbed37d90e70192d4690decae973d0f
made1993 <mariogr_93@hotmail.com>
2016-02-29 19:03:07 +0100

    commit makefile

--- P1/primera_parte/plots.r
+++ P1/primera_parte/plots.r
@@ -5,6 +5,6 @@ ta<-read.table("adaptacion.data")
 
 plot(t[,1][1:100], type="l", col="blue", ylim=c(0,1), ylab="adaptacion", xlab="etapas", main="p1 perceptrony adaline")
 lines(ta[,1], col="red")
-legend(x= 6000 , y=0.75,lty=c(1,1),lwd=c(2.5,2.5),col=c("blue","red"), legend=c("perceptron", "adaline"))
+legend(x= 60 , y=0.75,lty=c(1,1),lwd=c(2.5,2.5),col=c("blue","red"), legend=c("perceptron", "adaline"))
 rect(1, 5, 3, 7, col = "white")
 dev.off()
