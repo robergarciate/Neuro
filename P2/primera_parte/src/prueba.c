aa55942d438b79a0ff3236382c3353b4e3008d1b
robergarciate <roberto.garcia.teodoro@gmail.com>
2016-03-07 18:19:27 +0100

    dia 7

--- P2/primera_parte/src/prueba.c
+++ P2/primera_parte/src/prueba.c
@@ -5,12 +5,7 @@ double maxTolerancia=0.0;
 datos* adapt=NULL;
 int main(){
 	redNeuronal* r1=NULL;
-	redNeuronal* r2=NULL;
-	FILE* f=NULL;
-	datos* d =NULL;
-	f=fopen("bases-de-datos-p1/problema-real4.txt", "r");
-	d=leerDatos(f);
-	escribeDatos(d, "problema-real4.txt");
 
-	fclose(f);
+	r1=iniRedRetropropagacion( 5, 2, 2, 0.2);
+
 }
